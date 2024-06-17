5ad21a4787c71d877abc7ec0bfee540a5d8c2fbf
Matt Vella <mcvella@gmail.com>
2024-06-17 18:32:05 -0400

    initial commit, untested

--- README.md
+++ README.md
@@ -1,2 +1,119 @@
 # viam-numato-board-usb-android
-Viam modular component for Numato USB GPIO boards running on Android
+Viam modular component for Numato USB GPIO boards on Android devices
+
+# viam-numato-board-usb-android
+
+This module implements the [Viam generic component API](https://docs.viam.com/components/generic/) in an mcvella:generic:numato-board-usb-android model.
+With this component, you can control Numato USB GPIO devices in your Viam Android projects.
+
+## Requirements
+
+Viam server must be running on an Android device that has [OTG capabilities](https://en.wikipedia.org/wiki/USB_On-The-Go), with a [Numato](https://numato.com/) USB serial GPIO board device connected.
+
+Note that currently, the expectation is that this is the only USB serial device connected; it will assume the first serial device is a Numato GPIO board.
+
+## Build and Run
+
+To use this module, follow these instructions to [add a module from the Viam Registry](https://docs.viam.com/registry/configure/#add-a-modular-resource-from-the-viam-registry) and select the `mcvella:generic:numato-board-usb-android` model from the [`mcvella:generic:numato-board-usb-android` module](https://app.viam.com/module/mcvella/mcvella:generic:numato-board-usb-android).
+
+## Configure
+
+> [!NOTE]  
+> Before configuring your numato board component, you must [create a machine](https://docs.viam.com/manage/fleet/machines/#add-a-new-machine).
+
+Navigate to the **Config** tab of your robot’s page in [the Viam app](https://app.viam.com/).
+Click on the **Components** subtab and click **Create**.
+Select the `generic` type, then select the `mcvella:generic:numato-board-usb-android` model.
+Enter a name for your component and click **Create**.
+
+On the new component panel, copy and paste the following attribute template into your pubsub’s **Attributes** box:
+
+```json
+{
+}
+```
+
+> [!NOTE]  
+> For more information, see [Configure a Robot](https://docs.viam.com/manage/configuration/).
+
+### Attributes
+
+The following attributes are available for `mcvella:generic:numato-board-usb-android`:
+
+| Name | Type | Inclusion | Description |
+| ---- | ---- | --------- | ----------- |
+| |  |  |  |
+
+### Example Configurations
+
+A typical configuration might look like:
+
+```json
+{
+}
+```
+
+## API Usage
+
+DoCommand() can be used to set a GPIO digital pin high or low, read its current digital state, read its current analog state, as well as control software PWM.
+
+Example:
+
+``` python
+# set GPIO state to high on pin 2
+await board.do_command({'command': 'set_gpio', 'pin': 2, 'value': 'high'})
+# read analog on pin 3
+status = await board.do_command({'command': 'get_analog', 'pin': 3})
+# Set PWM frequency on pin 4 to 1600 Hz
+await board.do_command({'command': 'set_pwm_freq', 'pin': 4, 'value': 1600})
+# Get PWM duty cycle on pin 4
+await board.do_command({'command': 'get_pwm_duty', 'pin': 4})
+```
+
+Commands:
+
+### set_gpio
+
+*set_gpio* allows you to set the state of a GPIO pin on the Numato board.
+An integer value representing the index of a pin (starting with 0 for the first) must be passed as *pin*.
+*value* must be 'high' or 'low'
+
+### get_gpio
+
+*get_gpio* allows you to read the state of a GPIO pin on the Numato board.
+An integer value representing the index of a pin (starting with 0 for the first) must be passed as *pin*.
+The response will be 'high' or 'low'
+
+### get_analog
+
+*get_analog* allows you to read the state of an analog pin on the Numato board.
+An integer value representing the index of a pin (starting with 0 for the first) must be passed as *pin*.
+
+### set_pwm_freq
+
+*set_pwm_freq* allows you to set the PWM frequency on a pin (in Hz) on the Numato board.
+An integer value representing the index of a pin (starting with 0 for the first) must be passed as *pin*.
+*value* must be an integer representing PWM frequency in Hz
+
+### get_pwm_freq
+
+*get_pwm_freq* allows you to read the PWM frequency on a pin (in Hz) on the Numato board.
+An integer value representing the index of a pin (starting with 0 for the first) must be passed as *pin*.
+The response will be an integer representing PWM frequency in Hz
+
+### set_pwm_duty
+
+*set_pwm_duty* allows you to set the PWM duty cycle on a pin on the Numato board.
+An integer value representing the index of a pin (starting with 0 for the first) must be passed as *pin*.
+*value* must be an float representing PWM duty cycle as a float between 0 and 1, representing % of time in high state
+
+### get_pwm_duty
+
+*get_pwm_duty* allows you to read the PWM duty cycle on a pin on the Numato board.
+An integer value representing the index of a pin (starting with 0 for the first) must be passed as *pin*.
+The response will be a float representing PWM duty cycle as a float between 0 and 1, representing % of time in high state
+
+## TODO
+
+Currently, when a Numato device is plugged-in, you must allow device access via the Android screen within 5 seconds of first DoCommand() call.
+This could be improved by adding fixed intent for the device in the module.
---
+++ module/src/main/java/com/mcvella/numatoBoardUsbAndroid/Main.java
@@ -0,0 +1,239 @@
+package com.mcvella.numatoBoardUsbAndroid;
+
+import com.google.protobuf.Struct;
+import com.google.protobuf.Value;
+import com.viam.common.v1.Common;
+import com.viam.sdk.android.module.Module;
+import android.Manifest;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
+import android.content.Context;
+import android.content.Intent;
+import android.app.PendingIntent;
+import androidx.core.app.ActivityCompat;
+import android.content.pm.PackageManager;
+
+
+import com.viam.sdk.core.component.generic.Generic;
+import com.viam.sdk.core.resource.Model;
+import com.viam.sdk.core.resource.ModelFamily;
+import com.viam.sdk.core.resource.Registry;
+import com.viam.sdk.core.resource.Resource;
+import com.viam.sdk.core.resource.ResourceCreatorRegistration;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.List;
+import java.util.Set;
+import java.util.Arrays;
+import java.util.logging.Logger;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+
+import viam.app.v1.Robot;
+
+public class Main {
+
+  private static UsbManager usb;
+  private static Context cxt;
+  private static final String ACTION_USB_PERMISSION = "com.android.usb.USB_PERMISSION";
+  private static final String[] PERMISSIONS = {
+          Manifest.permission.ACCESS_COARSE_LOCATION,
+          Manifest.permission.ACCESS_FINE_LOCATION
+  };
+
+  public static void main(final String[] args) {
+
+    Registry.registerResourceCreator(
+        Generic.SUBTYPE,
+        NumatoBoard.MODEL,
+        new ResourceCreatorRegistration(NumatoBoard::new, NumatoBoard::validateConfig)
+    );
+    final Module module = new Module(args);
+    usb = (UsbManager) module.getParentContext().getSystemService(Context.USB_SERVICE);
+    cxt = module.getParentContext();
+    module.start();
+  }
+
+  public static class NumatoBoard extends Generic {
+
+    public static final Model MODEL = new Model(new ModelFamily("mcvella", "generic"), "numato-board-usb-android");
+    private static final Logger LOGGER = Logger.getLogger(NumatoBoard.class.getName());
+    private UsbSerialPort board;
+
+    public NumatoBoard(Robot.ComponentConfig config,
+        Map<Common.ResourceName, Resource> dependencies) {
+      super(config.getName());
+
+      // attempt to close on reconfigure.  note that connect is lazy and occurs when a doCommand() is issued
+      if ( board != null && board.isOpen()) {
+        try {
+          board.close();
+          board = null;
+        } catch (IOException e) {
+          LOGGER.warning("Could not disconnect Numato board device; perhaps was not already connected");
+        }
+      }
+
+    }
+
+    private boolean openBoard() {
+      List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(usb);
+      if (availableDrivers.isEmpty()) {
+        LOGGER.severe("No serial USB devices found");
+        return false;
+      }
+      LOGGER.warning("USB devices found:" + Arrays.toString(availableDrivers.toArray()));
+
+      UsbSerialDriver driver;
+      try {
+          driver = availableDrivers.get(0);
+      } catch (Exception ignored) {
+          LOGGER.severe("No correct Numato USB serial devices, could not connect");
+          return false;
+      }
+
+      // Check and grant permissions
+      if (!checkAndRequestPermission(usb, driver.getDevice())) {
+        LOGGER.severe("Please try again and grant permission to connect to Numato USB serial device");
+        return false;
+      }
+
+      // Open USB device
+      UsbDeviceConnection connection = usb.openDevice(driver.getDevice());
+      if (connection == null) {
+          LOGGER.severe("Error opening Numato USB device");
+          return false;
+      }
+
+      try {
+        // Open first available serial port
+        board = driver.getPorts().get(0);
+        board.open(connection);
+        board.setParameters(19200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+
+        LOGGER.info("Successfully connected to Numato board device");
+        return true;
+      } catch (IOException e) {
+        LOGGER.severe("Failure connecting to detected Numato board device");
+        return false;
+      }
+    }
+    public static Set<String> validateConfig(final Robot.ComponentConfig ignored) {
+      return new HashSet<>();
+    }
+
+    private boolean checkAndRequestPermission(UsbManager manager, UsbDevice usbDevice) {
+      // Check if permissions already exists
+      if (hasPermissions(cxt.getApplicationContext(), PERMISSIONS)
+              && manager.hasPermission(usbDevice))
+          return true;
+      else {
+          // Request USB permission
+          PendingIntent pendingIntent = PendingIntent.getBroadcast(cxt.getApplicationContext(),
+                  0, new Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_IMMUTABLE);
+          manager.requestPermission(usbDevice, pendingIntent);
+          
+          // this implies permissions must be accepted within 5 seconds of being asked
+          try {
+            Thread.sleep(5000);
+          } catch (InterruptedException e) {
+            return false;
+          }
+          return true;
+      }
+    }
+    public boolean hasPermissions(Context context, String... permissions) {
+      if (context != null && permissions != null) {
+          for (String permission : permissions) {
+              if (ActivityCompat.checkSelfPermission(context, permission)
+                      != PackageManager.PERMISSION_GRANTED) {
+                  return false;
+              }
+          }
+      }
+      return true;
+    }
+
+    private boolean ensureBoardOpen() {
+      Boolean needToConnect = false;
+
+      if (board == null)
+      {
+        needToConnect = true;
+      }
+      else {
+          try {
+            // test if connection is still responding.
+            // note that the isOpen() command could not reliably tell us this
+            byte[] resp = new byte[64];
+            board.write("\r".getBytes(), 100);
+            board.read(resp, 100);
+          } catch (IOException e) {
+            LOGGER.severe("Numato NEED TO CONNECT");
+
+            needToConnect = true;
+          }
+      }
+
+      if (needToConnect) {
+        Boolean open = openBoard();
+        if (!open) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    @Override
+    public Struct doCommand(Map<String, Value> command) {
+      final Struct.Builder builder = Struct.newBuilder();
+
+      if (!ensureBoardOpen()) {
+        LOGGER.severe("Unable to open Numato");
+        return builder.putFields("Unable to open board", Value.newBuilder().setStringValue("error").build()).build();
+      }
+
+      String serialCommand = "";
+      String type = "write";
+      if (command.containsKey("on")) {
+        serialCommand = "relay on " + command.get("on").getNumberValue() + "\r";
+      } else if (command.containsKey("off")) {
+        serialCommand = "relay off " + command.get("off").getNumberValue() + "\r";
+      } else if (command.containsKey("read")) {
+        serialCommand = "relay read " + command.get("read").getNumberValue() + "\r";
+        type = "read";
+      }
+
+      LOGGER.info("Numato serial command" + serialCommand);
+
+      if (type == "write") {
+        try {
+          board.write(serialCommand.getBytes(), 500);
+          return builder.putFields("serial command written", Value.newBuilder().setStringValue(serialCommand).build()).build();
+        } catch (IOException e) {
+          return builder.putFields("serial write error", Value.newBuilder().setStringValue(serialCommand).build()).build();
+        }
+      } else {
+        byte[] resp = new byte[64];
+        try {
+          board.write(serialCommand.getBytes(), 1000);
+          board.read(resp, 100);
+        } catch (IOException e) {
+          return builder.putFields("serial read error", Value.newBuilder().setStringValue(serialCommand).build()).build();
+        }
+        String response = new String(resp, StandardCharsets.UTF_8);
+        String rsplit[] = response.split("\\n\\r?");
+        // if the board was not yet used, default to "off"
+        String toReturn = rsplit.length > 1 ? rsplit[1] : "off";
+
+        return builder.putFields("status", Value.newBuilder().setStringValue(toReturn).build()).build();
+      }
+    }
+  }
+}
+
